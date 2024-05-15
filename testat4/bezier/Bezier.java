2564be722a33f2980f65d239d0cb030092248669
ElectroPulse <markus.grimmer@online.de>
2024-05-15 21:02:25 +0200

    ✨ testat4 aufgabe2a und aufgabe2b und styling

--- testat4/bezier/Bezier.java
+++ testat4/bezier/Bezier.java
@@ -1,38 +1,57 @@
-import java.awt.Graphics;
+package bezier;
+
+import java.awt.*;
 import java.util.List;
 
-class Bezier {
-	// TODO: Definieren Sie benötigte Attribute hier.
+class Bezier
+{
+    List<Point> points;
+    double h;
 
-	/**
-	 * Berechnet Beziér-Kurven. Der Grad der Beziér-Kurve ist über die Zahl der
-	 * Kontrollpunkte festgelegt.
-	 * 
-	 * @param points Kontrollpunkte.
-	 * @param h      Schrittweite beim Zeichnen der Beziér-Kurve
-	 */
-	Bezier(List<Point> points, double h) {
-		// TODO: Hier Ihr Code...
-	}
+    /**
+     * Berechnet Beziér-Kurven. Der Grad der Beziér-Kurve ist über die Zahl der
+     * Kontrollpunkte festgelegt.
+     *
+     * @param points Kontrollpunkte.
+     * @param h      Schrittweite beim Zeichnen der Beziér-Kurve
+     */
+    public Bezier(List<Point> points, double h)
+    {
+        this.points = points;
+        this.h = h;
+    }
 
-	/**
-	 * Berechne ein Punkt-Objekt, das die zweidimensionale Koordinate der
-	 * Bézier-Kurve für einen gegebenen Parameterwert errechnet.
-	 * 
-	 * @param t Kurvenparameter
-	 * @return Koordinate der Bézier-Kurve
-	 */
-	Point casteljau(double t) {
-		// TODO: Hier Ihr Code...
+    /**
+     * Berechne ein Punkt-Objekt, das die zweidimensionale Koordinate der
+     * Bézier-Kurve für einen gegebenen Parameterwert errechnet.
+     *
+     * @param t Kurvenparameter
+     * @return Koordinate der Bézier-Kurve
+     */
+    Point casteljau(double t)
+    {
+        Point[][] P = new Point[points.size()][points.size()];
+        for (int i = 0; i < points.size(); i++)
+        {
+            P[0][i] = points.get(i);
+        }
+        for (int k = 1; k < points.size(); k++)
+        {
+            for (int i = k; i < points.size(); i++)
+            {
+                P[i][k] = new Point((1 - t), P[k - 1][i - 1], t, P[k - 1][i]);
+            }
+        }
+        return P[points.size() - 1][points.size() - 1];
+    }
 
-	}
-
-	/**
-	 * Zeichne eine Bezier-Kurve mit Stütz- und Kontrollpunkten aus points.
-	 * 
-	 * @param graphics Grafikobjekt
-	 */
-	void render(Graphics graphics) {
-		// TODO: Hier Ihr Code...
-	}
+    /**
+     * Zeichne eine Bezier-Kurve mit Stütz- und Kontrollpunkten aus points.
+     *
+     * @param graphics Grafikobjekt
+     */
+    void render(Graphics graphics)
+    {
+        // TODO: Hier Ihr Code...
+    }
 }
