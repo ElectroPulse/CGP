e357eb3e8b8e6142035ef168c0b9adccf38b3a0f
ElectroPulse <markus.grimmer@online.de>
2024-05-22 13:51:08 +0200

    🚧 testat5 aufgabe1c+d

--- testat5/bspline/NonPeriodicUniformBSpline.java
+++ testat5/bspline/NonPeriodicUniformBSpline.java
@@ -7,5 +7,16 @@ public class NonPeriodicUniformBSpline extends BSpline
     public NonPeriodicUniformBSpline(List<Point> points, int k, double h)
     {
         super(points, k, h);
+        int step = 1;
+        knotVector = new double[points.size() + k];
+        for (int i = k; i < knotVector.length - k; i++)
+        {
+            knotVector[i] = step;
+            step++;
+        }
+        for (int i = knotVector.length - k; i < knotVector.length; i++)
+        {
+            knotVector[i] = step;
+        }
     }
 }
--- testat5/bspline/PeriodicUniformBSpline.java
+++ testat5/bspline/PeriodicUniformBSpline.java
@@ -7,5 +7,10 @@ public class PeriodicUniformBSpline extends BSpline
     public PeriodicUniformBSpline(List<Point> points, int k, double h)
     {
         super(points, k, h);
+        knotVector = new double[points.size() + k];
+        for (int i = 0; i < points.size() + k; i++)
+        {
+            knotVector[i] = i;
+        }
     }
 }
